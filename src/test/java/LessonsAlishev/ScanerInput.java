9861863b2e6ed4ab4a12bda18b3367621a38919c
Dmytro Uliatovskyi <Dmytro.Uliatovskyi@cchellenic.com>
2022-01-25 03:16:23 +0200

    Alishev

---
+++ src/test/java/LessonsAlishev/ScanerInput.java
@@ -0,0 +1,13 @@
+package LessonsAlishev;
+
+import java.util.Scanner;
+
+public class ScanerInput {
+    public static void main(String [] args){
+        Scanner s = new Scanner(System.in);
+        System.out.println("enter: ");
+        String string = s.nextLine();
+        System.out.println("You enter "+string);
+
+    }
+}
